bb0cb39dfde1d51f38ecb656432fc2e5df272a3d
mili <kamil.maksudov2019@gmail.com>
2023-04-02 03:25:34 +0200

    feat: automation 02

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -3,8 +3,8 @@ import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
 	// Start
-	// const start = false;
-	const start = true;
+	const start = false;
+	// const start = true;
 
 	if (!start) return;
 	// Open a browser
@@ -15,6 +15,9 @@ export const load = (async () => {
 		bypassCSP: true
 	});
 	await page.goto('https://otv.verwalt-berlin.de/ams/TerminBuchen');
+	// await page.goto(
+	// 	'https://otv.verwalt-berlin.de/ams/TerminBuchen/wizardng/c7370d8c-25a1-4df1-9cb8-84f12db1b220?dswid=6185&dsrid=692&st=3&v=1680396726421'
+	// );
 
 	// Find a button called "Termin buchen" and click it
 	await page.getByRole('link', { name: 'Termin buchen' }).click();
@@ -26,7 +29,7 @@ export const load = (async () => {
 	await page.getByRole('button', { name: 'Weiter' }).click();
 
 	// Wait for 3000ms
-	await page.waitForTimeout(5000);
+	await page.waitForTimeout(3000);
 
 	// Find a select element sel_staat and select the option "Chile"
 	await page.selectOption('select[name="sel_staat"]', 'Chile');
@@ -50,87 +53,78 @@ export const load = (async () => {
 	// ID services
 	await page.click(`label[for="SERVICEWAHL_DE3${selectedOptionValue}-0-1"]`, { force: true });
 
-	/* ID main reasons
-	 * SERVICEWAHL_DE_332-0-1-3 - Studium oder Ausbildung
-	 * SERVICEWAHL_DE_332-0-1-1 - Erwerbstätigkeit
-	 * SERVICEWAHL_DE_332-0-1-4 - Familiäre Gründe
-	 * SERVICEWAHL_DE_332-0-1-6 - Besondere Aufenthaltsrechte
-	 */
+	//  ID main reasons
+	//   SERVICEWAHL_DE_332-0-1-3 - Studium oder Ausbildung
+	//   SERVICEWAHL_DE_332-0-1-1 - Erwerbstätigkeit
+	//   SERVICEWAHL_DE_332-0-1-4 - Familiäre Gründe
+	//   SERVICEWAHL_DE_332-0-1-6 - Besondere Aufenthaltsrechte
 
+	console.log('Start');
 	await page.waitForTimeout(1000);
+	const level2Labels = await page.$$('div.level2 > label');
+	await Promise.all(
+		level2Labels.map(async (level2Label) => {
+			const level2LabelForAttr = await level2Label.getProperty('htmlFor');
+			const level2LabelFor = await level2LabelForAttr.jsonValue();
+			try {
+				await page.waitForSelector(`label[for="${level2LabelFor}"]`, { timeout: 5000 });
+				await page.click(`label[for="${level2LabelFor}"]`, { force: true });
+				await page.waitForTimeout(2000);
 
-	const services = await page.$$('div.level2 > label');
-	for (const service of services) {
-		if (!service.serviceForAttr) continue;
-		const serviceForAttr = await service.getProperty('htmlFor');
-		const serviceFor = await serviceForAttr.jsonValue();
-		await page.click(`label[for="${serviceFor}"]`, { force: true });
-		await page.waitForTimeout(3000);
-
-		const options = await page.$$('div.level3 > label');
-		await Promise.all(
-			options.map(async (option) => {
-				if (!option) return;
-				const optionForAttr = await option.getProperty('htmlFor');
-				const optionFor = await optionForAttr.jsonValue();
-				await page.click(`label[for="${optionFor}"]`, { force: true });
-				await page.getByRole('button', { name: 'Weiter' }).click();
-				await page.waitForTimeout(3000);
-			})
-		);
-		await page.waitForTimeout(3000);
-	}
-
-	// const nestedLabels = await page.$$('div.level2 > label');
-
-	// left arrow ui-datepicker-prev ui-corner-all
-	// right arrow ui-datepicker-next ui-corner-all
-
-	console.log('Choose date');
-
-	await page.waitForTimeout(3000);
-
-	const tables = await page.$$('table');
-	console.log(tables);
-	const tablesWithAnchors = await Promise.all(
-		tables.map(async (table) => {
-			const anchors = await table.$$eval('a', (anchors) => anchors.map((anchor) => anchor.href));
-			return anchors;
+				const level3Labels = await page.$$('div.level3 > label');
+				await Promise.all(
+					level3Labels.map(async (level3Label) => {
+						const level3LabelForAttr = await level3Label.getProperty('htmlFor');
+						const level3LabelFor = await level3LabelForAttr.jsonValue();
+						try {
+							await page.waitForSelector(`label[for="${level3LabelFor}"]`, { timeout: 5000 });
+							await page.click(`label[for="${level3LabelFor}"]`, { force: true });
+							await page.getByRole('button', { name: 'Weiter' }).click();
+							await page.waitForTimeout(3000);
+						} catch (error) {
+							console.error(`Error clicking label with for attribute ${level3LabelFor}: ${error}`);
+						}
+					})
+				);
+				await page.waitForTimeout(2000);
+			} catch (error) {
+				console.error(`Error clicking label with for attribute ${level2LabelFor}: ${error}`);
+			}
 		})
 	);
+	console.log('Table');
 
-	console.log(tablesWithAnchors);
+	// Find the table element
+	const tableElement = await page.$('table');
+	await page.waitForSelector('table');
+	// If the table element is not found, throw an error
+	if (!tableElement) throw new Error('Table element not found');
 
-	// for (const anchor of tds) {
-	// 	console.log(anchor);
-	// 	const href = await anchor.getProperty('href');
-	// 	console.log(await href.jsonValue());
-	// }
-	// await page.waitForTimeout(2000);
+	// Find the anchor link within the table
+	const anchorLink = await tableElement.$('a');
 
-	// await page.selectOption('select[name="dd_zeiten"]', '07:30');
-	// await page.getByRole('button', { name: 'Weiter' }).click();
-	// await page.waitForTimeout(3000);
+	if (!anchorLink) throw new Error('Table element not found');
+	// Click on the anchor link
+	await anchorLink.click();
 
-	/*
-	 * Specific reasons
-	 * SERVICEWAHL_DE332-0-1-3-305244 - Aufenthaltserlaubnis zum Studium (§ 16b)
-	 */
-	// await page.click(`label[for="SERVICEWAHL_DE${selectedOptionValue}-0-1-3-328338"]`, {
-	// 	force: true
-	// });
+	// Find a select element dd_zeiten and select the first option
+	await page.selectOption('select[name="dd_zeiten"]', { index: 0 });
+	await page.getByRole('button', { name: 'Weiter' }).click();
+	await page.waitForTimeout(3000);
 
-	// Find a button called "Weiter" and click it
-	// await page.getByRole('button', { name: 'Weiter' }).click();
+	// Fill the input element with the name "antragsteller_vname"
+	await page.fill('input[name="antragsteller_vname"]', 'Johny');
+	await page.fill('input[name="antragsteller_nname"]', 'Walter');
+	await page.selectOption('select[name="sel_aufenthaltserlaubnis"]', 'Nein');
+	await page.fill('input[name="antragsteller_email"]', 'walterrjohny@gmail.com');
+	await page.fill('input[name="antragsteller_gebDatum"]', '11.01.1995');
+	await page.keyboard.press('Tab');
+	// await page.fill('input[name="sel_aufenthaltserlaubnis"]', 'Ja');
+	// await page.fill('input[name="antragsteller_nrAufenthaltserlaubnis"]', '12345');
+	await page.getByRole('button', { name: 'Weiter' }).click();
+	await page.waitForTimeout(3000);
 
-	// Wait for the page to load
-	// await page.waitForLoadState('networkidle');
-	// await page.waitForSelector('li.errorMessage');
-	// if (await page.isVisible('li.errorMessage'))
-	// 	// Close the browser
-	// 	await browser.close();
-
-	// console.log(await page.isVisible('li.errorMessage'));
+	await page.getByRole('button', { name: 'Termin buchen' }).click();
 
 	return {
 		status: 200
