3f054a7ecf55992f8a1c92710fdbd91400b76d73
mili <kamil.maksudov2019@gmail.com>
2023-04-09 19:50:18 +0200

    feat: vercel analytics

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,8 @@
 import type { LayoutServerLoad } from './$types';
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load: LayoutServerLoad = async (event) => {
 	return {
