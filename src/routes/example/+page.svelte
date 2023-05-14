dd5eb3d27b2c3e30a7064e66b97aa47d8e2d26bd
mili <kamil.maksudov2019@gmail.com>
2023-05-14 11:26:36 +0200

    feat: add apexcharts

--- src/routes/example/+page.svelte
+++ src/routes/example/+page.svelte
@@ -1,2 +1,30 @@
-<script lang="ts">
+<script>
+	import ApexCharts from 'apexcharts';
+	import { onMount } from 'svelte';
+
+	let chart;
+
+	export let options = {
+		chart: {
+			type: 'line'
+		},
+		series: [
+			{
+				name: 'Series 1',
+				data: [30, 40, 25, 50, 49, 21, 70, 51]
+			}
+		],
+		xaxis: {
+			categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug']
+		}
+	};
+
+	export let id = 'chart';
+
+	onMount(() => {
+		chart = new ApexCharts(document.querySelector(`#${id}`), options);
+		chart.render();
+	});
 </script>
+
+<div class="w-full" {id} />
